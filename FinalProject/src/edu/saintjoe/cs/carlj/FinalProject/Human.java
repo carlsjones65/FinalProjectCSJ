fdcb38e264bcb3f0d2a33649d7f00d3658d23674
carlsjones65 <cjones2@saintjoe.edu>
2015-05-08 17:58:18 -0400

    Saving the modifications to mammal and human

--- FinalProject/src/edu/saintjoe/cs/carlj/FinalProject/Human.java
+++ FinalProject/src/edu/saintjoe/cs/carlj/FinalProject/Human.java
@@ -1,12 +1,39 @@
 package edu.saintjoe.cs.carlj.FinalProject;
 
-public class Human extends Mammal {
+public class Human {
 	
-	public String name;
+public final String name;
 	
-	public Object Language;
+	public String Language;
 	
+	/*----------------------------------------------*/
+	//Bonus:
+	//this is an example of overriding because mammal
+	//has the exact same method in its code with 
+	//the same name
+	// this is also an example of overloading too
+	//because C1 and C2 are in the same class, have
+	//the same name, but have different signatures
+	//C1
+	public Human(String name) {
+		this.name = name;
+		}
 	
+	//C2
+	public Human(String name, String Language) {
+		this.name = name;
+		this.Language = Language;
+		}
 	
+	public String getname() {
+		return name;
+		
+	public String getLanguage() {
+		return Language;
+		}
+		
+	}
+	
+
 
 }
--- FinalProject/src/edu/saintjoe/cs/carlj/FinalProject/Mammal.java
+++ FinalProject/src/edu/saintjoe/cs/carlj/FinalProject/Mammal.java
@@ -7,11 +7,15 @@ public class Mammal {
 	public Object Lifespan;
 	
 	/*--------------------------------------------------------*/
-	
+	//Bonus:
+	//this is an example of overloading, C1 and C2 have
+	//the same name, different signatures, and are in 
+	//the same class
+	//C1
 	public Mammal(String name) {
 		this.name = name;
 		}
-	
+	//C2
 	public Mammal(String name, Object Lifespan) {
 		this.name = name;
 		this.Lifespan = Lifespan;
